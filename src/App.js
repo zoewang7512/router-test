1cd1d4e91a49cafa43972d8fd1ecb8491a19a925
zoewang7512 <zoewang7512@gmail.com>
2024-06-10 01:01:08 +0800

    add 404

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
       <Routes>
            <Route path="/" element={<Home />} />
             <Route path="/page1" element={<Page1 />} />
-            <Route path='*' element={<ErrorPage />}/>
+            {/*<Route path='*' element={<ErrorPage />}/>*/}
+            <Route path='/404' element={<ErrorPage/>} />
+            <Route path='*' element={<Navigate replace to='/404'/>} />
           </Routes></HashRouter>
     </div>
   );
