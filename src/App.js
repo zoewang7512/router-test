bc17fa7633ac82c721213ad06b68eca21c1a29bd
zoewang7512 <zoewang7512@gmail.com>
2024-06-10 01:02:46 +0800

    add Navigate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route,Navigate } from "react-router-dom";
 import './App.css';
 
 import Home from "./Home";
